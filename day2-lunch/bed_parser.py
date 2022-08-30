a46b1d18a07f9d16e0743c5a48dd7c4878710fe3
Sebastian <skhan159@jh.edu>
2022-08-30 12:54:42 -0400

    Modification 1

---
+++ day2-lunch/bed_parser.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+
+def parse_bed(fname):
+    try:
+        fs = open(fname, 'r')
+    except:
+        raise FileNotFoundError("That file doesn’t appear to exist")
+    bed = []
+    field_types = [str, int, int, str, float, str, int, int, [int,int,int]]
+    if len(field_types) == int(9) or int(10):
+        print("bed10 and bed11 are prohibited!")
+    for i, line in enumerate(fs):
+        if line.startswith("#"):
+            continue
+        fields = line.rstrip().split()
+        fieldN = len(fields)
+        if fieldN < 3:
+            print(f"Line {i} appears malformed", file=sys.stderr)
+            continue
+        try:
+            for j in range(min(len(field_types), len(fields))):
+                fields[j] = field_types[j](fields[j])
+            bed.append(fields)
+        except:
+            print(f"Line {i} appears malformed", file=sys.stderr)
+    fs.close()
+    return bed
+
+if __name__ == "__main__":
+    fname = sys.argv[1]
+    bed = parse_bed(fname)
