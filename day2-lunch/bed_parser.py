805b40057319b6c95d87e7938f0adcd32df3156f
Sebastian <skhan159@jh.edu>
2022-08-30 15:11:16 -0400

    modified, extended script

--- day2-lunch/bed_parser.py
+++ day2-lunch/bed_parser.py
@@ -8,20 +8,29 @@ def parse_bed(fname):
     except:
         raise FileNotFoundError("That file doesn’t appear to exist")
     bed = []
-    field_types = [str, int, int, str, float, str, int, int, [int,int,int]]
-    if len(field_types) == int(9) or int(10):
-        print("bed10 and bed11 are prohibited!")
+    field_types = [str, int, int, str, int, str, int, int]
     for i, line in enumerate(fs):
         if line.startswith("#"):
             continue
         fields = line.rstrip().split()
         fieldN = len(fields)
-        if fieldN < 3:
+        if fieldN < 3 or fieldN == 10 or fieldN == 11:
             print(f"Line {i} appears malformed", file=sys.stderr)
             continue
         try:
-            for j in range(min(len(field_types), len(fields))):
-                fields[j] = field_types[j](fields[j])
+            for j in range(fieldN):
+                if j in range(0, 8):
+                    fields[j] = field_types[j](fields[j])
+                elif j == 8:
+                    fields[j] = fields[j].split(",")
+                    fields[j] = [int(i) for i in fields[j]]
+                    assert len(fields[8]) == 3
+                elif j == 9:
+                    fields[j] = int(fields[j])
+                elif j == 10 or 11:
+                    fields[j] = fields[j].rstrip(",").split(",")
+                    fields[j] = [int(i) for i in fields[j]]
+                    assert fields[9] == len(fields[j])
             bed.append(fields)
         except:
             print(f"Line {i} appears malformed", file=sys.stderr)
