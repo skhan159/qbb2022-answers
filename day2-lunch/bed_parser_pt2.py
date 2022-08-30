1f9d3774f3464c8fb9f884fbf8d9165f587e3549
Sebastian <skhan159@jh.edu>
2022-08-30 18:28:38 -0400

    updated with annotations

--- day2-lunch/bed_parser_pt2.py
+++ day2-lunch/bed_parser_pt2.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-import statistics
-import bed_parser
+import statistics #stats for median
+import bed_parser #former script call
 fname = sys.argv[1]
-bed = bed_parser.parse_bed(fname)
-medvals = []
-lengthbed = len(bed)
+bed = bed_parser.parse_bed(fname) #define variable as the script result
+medvals = [] #list of median variables
+lengthbed = len(bed) #length of former script result
 for i in range(0, lengthbed):
-    medvals.append(int(bed[i][9]))
+    medvals.append(int(bed[i][9])) #median of exons per gene
 print(statistics.median(medvals))
 #wc -l sys.argv[1] = gene count
 #in our case, that is 3273
