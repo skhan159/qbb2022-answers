5cd9f33d6a94fea2511c1f0ae76c8c9aa7f3c5a1
Sebastian <skhan159@jh.edu>
2022-09-16 00:00:19 -0400

    commented script

--- week1-homework/week_1.py
+++ week1-homework/week_1.py
@@ -6,15 +6,15 @@ import seaborn as sns
 from statsmodels.stats.multitest import multipletests
 
 array = numpy.zeros(1000000)
-for i in range(50000):
+for i in range(50000): #this range can be altered for the set number of reads required for a genome at a specific depth. In this case, it is set for 100bp reads of a 1Mbp genome at 5x depth
     start = numpy.random.randint(0, high=999900)
     for h in range(start, start+100):
         array[h] += 1
 
 fig, ax = plt.subplots()
 ax.hist(array, label = "Distribution")
-x = numpy.arange(0, 50, 1)
-y = poisson.pmf(x, 5)*1000000
+x = numpy.arange(0, 50, 1) #this range is arbitrary, but can be changed to fit the data better
+y = poisson.pmf(x, 5)*1000000 #the second poisson call is lambda. Here it is set at 5, which is the depth of coverage specified before in the array
 ax.plot(x, y, label = "Poisson")
 ax.set_xlabel("Number of reads")
 ax.set_ylabel("Coverage of reads")
@@ -26,13 +26,9 @@ count = 0
 for u in array:
     if u == 0:
         count += 1
-print(count)
+print(count) #this bit of code is used to track the locations of zero coverage in the array
 
 lol = poisson.pmf(x, 5)*1000000
-print(lol)
-# count2 = 0
-# for l in lol:
-#     if l == 0:
-#         count2 += 1
-# print(count2)
+print(lol) #this bit of code is used to track the poisson predicted locations of zero coverage. The first number of this printed array is the zero coverage.
+
 
