fea31a8da3fa9a58f9b1b9fb614b1814e9ffd99a
Sebastian <skhan159@jh.edu>
2022-10-07 14:23:32 -0400

    updated script

--- week4-homework/gwas_data/GWAS.py
+++ week4-homework/gwas_data/GWAS.py
@@ -4,7 +4,7 @@ import numpy as np
 import vcfParser
 
 pca = np.genfromtxt(f"{sys.argv[1]}", encoding = None, skip_header=1, names= ["chr", "snp", "bp", "A1", "TEST", "NMISS", "BETA", "STAT", "P"])
-
+IC50 = np.genfromtxt(f"{sys.argv[3]}", encoding = None, skip_header=1, names= ["FID", "IID", "IC50"])
 fig, ax = plt.subplots()
 pvals = pca["P"]
 value = 10e-5
@@ -22,9 +22,24 @@ ax.set_title(f"{sys.argv[1]} SNPs")
 genotypes = vcfParser.parse_vcf(sys.argv[2])
 
 big_SNP = "rs7257475"
-index = np.where(genotypes[:] == big_SNP)
-matching_snp = genotypes[index]
-print(matching_snp)
+homo_rec = []
+het = []
+homo_dom = []
+for q in genotypes:
+    if q[2] == big_SNP:
+        line = q
+        for u in line:
+            if u == "0/0":
+                count = 1
+                homo_dom.append(IC50["IC50"[count]])
+                count += 1
+            elif u == "0/1":
+                het.append("1")
+            elif u == "1/1":
+                homo_dom.append("1")
+print(homo_dom)
+
+
 
 
 
